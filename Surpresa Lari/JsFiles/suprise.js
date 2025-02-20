00bc4ebba4b3f08a30b45391593daa199d250a2a
Laura Santoro Cruz <132526523+Daylighter04@users.noreply.github.com>
2025-02-20 02:33:16 -0300

    Add files via upload

---
+++ Surpresa Lari/JsFiles/suprise.js
@@ -0,0 +1,132 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let btnNao = document.getElementById("nao");
+
+    btnNao.addEventListener("mouseover", function() {
+        let x = Math.random() * window.innerWidth - 100;
+        let y = Math.random() * window.innerHeight - 50;
+        btnNao.style.left = `${x}px`;
+        btnNao.style.top = `${y}px`;
+    });
+});
+
+function mostrarSurpresa() {
+    document.getElementById("popup").style.display = "flex";
+    startExplosion(); // Chama a função de explosão assim que o popup aparece
+}
+
+function fecharPopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
+var emitterSize = 20,
+dotQuantity = 40,
+dotSizeMin = 6,
+dotSizeMax = 8,
+speed = 2.4,
+gravity = 0.7,
+explosionQuantity = 5,
+emitter = document.querySelector('.emitter'),
+explosions = [],
+currentExplosion = 0,
+container, i;
+
+function createExplosion(container) {
+  var tl = new TimelineLite({paused: true}),
+  dots = [],
+  angle, duration, length, dot, i, size, r, g, b;
+  for (i = 0; i < dotQuantity; i++) {
+    dot = document.createElement('div');
+    dots.push(dot);
+    dot.className = 'dot';
+    r = getRandom(30, 255);
+    g = getRandom(30, 230);
+    b = getRandom(30, 230);
+    TweenLite.set(dot, {
+      backgroundColor: 'rgb('+r+','+g+','+b+')',
+      visibility: 'hidden'
+    });
+    size = getRandom(dotSizeMin, dotSizeMax);
+    container.appendChild(dot);
+    angle = getRandom(0.65, 0.85) * Math.PI * 2;
+    length = Math.random() * (emitterSize / 2 - size / 2);
+    duration = 3 + Math.random();
+    TweenLite.set(dot, {
+      x: Math.cos(angle) * length, 
+      y: Math.sin(angle) * length, 
+      width: size, 
+      height: size, 
+      xPercent: -50, 
+      yPercent: -50,
+      visibility: 'hidden',
+      force3D: true
+    });
+    tl.to(dot, duration / 2, {
+      opacity: 0,
+      ease: RoughEase.ease.config({
+        points: 20,
+        strength: 1.75,
+        clamp: true
+      })
+    }, 0).to(dot, duration, {
+      visibility: 'visible',
+      rotationX: '-='+getRandom(720, 1440),
+      rotationZ: '+='+getRandom(720, 1440),
+      physics2D: {
+        angle: angle * 180 / Math.PI, 
+        velocity: (100 + Math.random() * 250) * speed, 
+        gravity: 700 * gravity,
+        friction: getRandom(0.1, 0.15)
+      }
+     }, 0).to(dot, 1.25 + Math.random(), {
+      opacity: 0
+    }, duration / 2);
+  }
+  return tl;
+}
+
+function explode(element) {
+  var bounds = element.getBoundingClientRect(),
+  explosion;
+  if (++currentExplosion === explosions.length) {
+    currentExplosion = 0;
+  }
+  explosion = explosions[currentExplosion];
+  TweenLite.set(explosion.container, {
+    x: bounds.left + bounds.width / 2,
+    y: bounds.top + bounds.height / 2
+  });
+  explosion.animation.restart();
+}
+
+function getRandom(min, max) {
+  var rand = min + Math.random() * (max - min);
+  return rand;
+}
+
+function startExplosion() {
+  for (i = 0; i < explosionQuantity; i++) {
+    container = document.createElement('div');
+    container.className = 'dot-container';
+    document.body.appendChild(container);
+    explosions.push({
+      container: container,
+      animation: createExplosion(container)
+    });
+  }
+
+  setTimeout(function() {
+    play(); // Agora chama a função play após a inicialização das explosões
+  }, 500); // Espera um pequeno intervalo para garantir que o pop-up esteja visível
+}
+
+function play() {
+  var intervalCount = 0,
+  interval = setInterval(function() {
+    if (intervalCount < 5) {
+      explode(emitter);
+      intervalCount++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 150);
+}
